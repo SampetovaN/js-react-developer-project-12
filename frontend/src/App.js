715d506d121adc5339aca186406ab38b2856c109
SampetovaN <nikasampetova@mail.ru>
2024-04-15 21:39:55 +0300

    fixes step 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SignupForm from './Components/SignupForm';
+import Login from './Components/Login';
 import Chat from './Components/Chat';
 import NotFound from './Components/NotFound';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="login" element={<SignupForm />} />
+      <Route path="login" element={<Login />} />
       <Route path="/" element={<Chat />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,28 +1,49 @@
-import { useFormik } from 'formik';
+import {useFormik} from 'formik';
+import cn from 'classnames';
 
 const SignupForm = () => {
   const formik = useFormik({
     initialValues: {
-      email: '',
+      password: '',
       username: '',
     },
+    onSubmit: (values) => {
+      // todo
+    },
+    validate: (values) => {
+      const errors = {};
+
+      if (!values.password) {
+        errors.password = 'Обязательное поле';
+      }
+      if (!values.username) {
+        errors.username = 'Обязательное поле';
+      }
+
+      return errors;
+    },
   });
+  const inputClasses = (error) => cn('form-control', {'is-invalid': error})
   return (
     <div className="row justify-content-center align-content-center h-100">
       <div className="col-12 col-md-8 col-xxl-6">
+        <h1 className="text-center mb-4">Войти</h1>
         <form onSubmit={formik.handleSubmit}>
           <h1 className="text-center mb-4">Войти</h1>
           <div className="form-floating mb-4">
             <input
               id="username"
               name="username"
-              type="username"
+              type="text"
               onChange={formik.handleChange}
               value={formik.values.username}
-              className="form-control"
+              className={inputClasses(formik.errors.username)}
               placeholder="Ваш ник"
             />
             <label htmlFor="username">Ваш ник</label>
+            <div className="invalid-feedback is-invalid">
+              {formik.errors.username}
+            </div>
           </div>
           <div className="form-floating mb-4">
             <input
@@ -31,10 +52,13 @@ const SignupForm = () => {
               type="password"
               onChange={formik.handleChange}
               value={formik.values.password}
-              className="form-control"
+              className={inputClasses(formik.errors.password)}
               placeholder="Пароль"
             />
-            <label htmlFor="password">Пароль</label>
+            <label htmlFor="password" className="is-invalid">Пароль</label>
+            <div className="invalid-feedback">
+              {formik.errors.password}
+            </div>
           </div>
           <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
         </form>
